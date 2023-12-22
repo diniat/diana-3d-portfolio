be2c55525bc0bf71ba34483545535d2795669d21
diniat <dianatobe@gmail.com>
2023-12-22 02:23:43 -0300

    replaced jpg image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -7,7 +7,7 @@ import logo from "./logo.svg";
 import hoooman from "./hoooman-st.png";
 import first from "./1.png";
 import second from "./2.png";
-import third from "./3.JPG";
+import third from "./3.png";
 import fourth from "./4.png";
 
 export {
