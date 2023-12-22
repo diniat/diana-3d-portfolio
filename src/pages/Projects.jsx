a471580f165ee9cfa50ba3b538e28b85cd82566c
diniat <dianatobe@gmail.com>
2023-12-22 17:45:50 -0300

    fix image size

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -32,7 +32,7 @@ const Project = ({ title, type, img, link, github, hidden }) => {
         className="absolute  top-0 -right-3 -z-10 h-[103%] w-[102%] rounded-[2rem] rounded-br-3xl md:-right-2 md:w-[101%] xs:h-[102%]
         xs:rounded-[1.5rem]  "
       />
-      <div className="w-full relative overflow-hidden rounded-lg h-20 aspect-video">
+      <div className="w-full relative overflow-hidden rounded-lg h-full aspect-video">
         <img src={img} alt={title} fill className="object-cover aspect-video" />
       </div>
       <div className="mt-4 flex w-full flex-col items-start justify-between">
